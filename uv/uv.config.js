797b5dea854c8bdbdee94eaccf60455e591d8c1d
qatual <central@soloo.win>
2025-07-23 18:31:18 -0400

    hai

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
     self.__uv$config = {
-        prefix: '/static/roman/',
+        prefix: '/static/solo/',
         bare: "/bare/",
         encodeUrl: Ultraviolet.codec.xor.encode,
         decodeUrl: Ultraviolet.codec.xor.decode,
